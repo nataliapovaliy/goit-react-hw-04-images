da448f63a9bf616d2de67ad7518f3bf95a6c0dca
nataliapovaliy <natalia.bg1125@gmail.com>
2023-01-24 13:34:58 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+
+  state {
+  
+  }
+  
 };
